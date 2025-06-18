89909df2ebe1685cc109e3c7245ae34bfc28f62d
HappyDie77 <soodenny123@gmail.com>
2025-06-18 22:21:06 +0200

    RFEDDFGTHY

--- Deny/Scripts/skills_player.gd
+++ Deny/Scripts/skills_player.gd
@@ -2,7 +2,7 @@ extends Node3D
 
 @onready var skills: Node3D = $"."
 
-const SKILL_1 = preload("res://Deny/Scenes/skill_1.tscn")
+const SLASH_1 = preload("res://Deny/Scenes/slash_1.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -15,7 +15,8 @@ func _process(delta: float) -> void:
 
 func _unhandled_input(event):
 	if event.is_action_pressed("Skill 1"):
-		var temp_skill_1 = SKILL_1.instantiate()
+		#FIX LATER TO MAKE MODULAR LIKE CONTAINER AND INV
+		var temp_skill_1 = SLASH_1.instantiate()
 		skills.add_child(temp_skill_1)
 		Global.skill_1 = true
 		print("PRess 1")
